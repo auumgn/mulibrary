03cb6b5b25bc26d8f002146b717639d3db1429af
andrej romanov <50377758+auumgn@users.noreply.github.com>
2025-09-07 23:53:23 +0300

    restructure repo

--- client/src/app/shared/models/category.model.ts
+++
@@ -1,9 +0,0 @@
-export interface ICategory {
-    name: string;
-    id?: number;
-    color?: string;
-}
-
-export class Category implements ICategory {
-    constructor(public name: string, public id?: number, public color?: string) { }
-}
--- server/readme.md
+++
@@ -1,6 +0,0 @@
-FETCH FROM LASTFM
-npx tsx .\src\services\lastfm-scan.ts
-
-
-SCAN LOCAL LIBRARY
-npx tsx .\src\services\local-scan.ts
