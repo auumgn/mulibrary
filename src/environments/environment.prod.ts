0c06fbc251ad662f1601c84cbec0efc4d84c0e7f
andrej romanov <50377758+auumgn@users.noreply.github.com>
2025-09-08 00:11:19 +0300

    fix env vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
   supabaseUrl: 'https://tryvbymzjvqlhjvuvuqi.supabase.co',
   supabaseKey: 'sb_publishable_r5-Y1zqQWu8XOvEKJp6P8g_nPWY9x_f'
 };
